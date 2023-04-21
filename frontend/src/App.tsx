19c59f6d4edaf4ee56a27cc1d8bdf7ae5444d2d1
unknown <toropov.nikita2004@gmail.com>
2023-04-21 22:11:19 +0200

    feat: add background to the main page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,6 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 
-
-
 function App() {
 
   return (
--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import "../css/Home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div id="home-page">
+      <nav></nav>
+      <video id="background-video" autoPlay muted loop playsInline>
+        <source src="/FinalVideo.mp4"></source>
+      </video>
+    </div>
   )
 }
 
