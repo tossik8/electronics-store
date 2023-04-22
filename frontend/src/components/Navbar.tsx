ad2b319811751aa5e8087ac24ebc673b26a58722
unknown <toropov.nikita2004@gmail.com>
2023-04-22 14:58:56 +0200

    feat: add logo on the page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import "../css/Navbar.css";
 
 const Navbar = () => {
   return (
     <header>
         <nav>
-            
+            <img id="logo" src="/star-tech-website-favicon-color.png" alt="Logo."/>
         </nav>
         <h1 id="title">Star Tech</h1>
     </header>
