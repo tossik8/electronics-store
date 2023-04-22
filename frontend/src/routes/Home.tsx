e177fa78447bf0e3b6d5da22af6f19afa135a0d9
unknown <toropov.nikita2004@gmail.com>
2023-04-22 19:55:29 +0200

    feat: add a toggle sound button

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../css/Home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeXmark, faVolumeUp } from "@fortawesome/free-solid-svg-icons";
 import Navbar from '../components/Navbar';
 
 const Home = () => {
+  const [isMuted, setIsMuted] = useState(true);
+
+  const handleClick = () => {
+    const video = document.getElementById("background-video") as HTMLVideoElement;
+    video!.muted = !video?.muted
+    setIsMuted(!isMuted);
+  }
+
   return (
     <div id="home-page">
+      <button id="sound-button" className="toggle-sound" onClick={handleClick}>{isMuted? <FontAwesomeIcon icon={faVolumeUp} />:<FontAwesomeIcon icon={faVolumeXmark}/>}</button>
       <Navbar/>
       <video id="background-video" autoPlay muted loop playsInline>
         <source src="/FinalVideo.mp4"></source>
