956b453ac628b3256406e7a6fb88b3981e4d97b8
unknown <toropov.nikita2004@gmail.com>
2023-04-23 11:43:06 +0200

    feat: make the input field be controlled

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../css/Navigation.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass, faCartShopping, faUser, faXmark } from "@fortawesome/free-solid-svg-icons"
+import { useNavigate } from 'react-router-dom';
 
 const Navigation = () => {
+  const [input, setInput] = useState<string>("");
+  let navigation = useNavigate();
+
+  const handleSearch = () => {
+    
+  }
+
   return (
     <section id="navigation-section">
       <nav className="navigation-panel">
         <img id="logo" src="/star-tech-logo.png" alt="Logo."/>
         <div className="search-bar-div">
-          <button className="button-icon search"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
-          <input className="item-search" type="text" />
-          <button className="button-icon delete" ><FontAwesomeIcon icon={faXmark} /></button>
+          <button type="button" className="button-icon search" onClick={handleSearch}><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+          <input className="item-search" type="text" onChange={e => setInput(e.target.value)} value={input}/>
+          <button type="button" className="button-icon delete" onClick={() => setInput("")} ><FontAwesomeIcon icon={faXmark} /></button>
         </div>
         <div className="icons">
-          <button className="button-icon cart"><FontAwesomeIcon icon={faCartShopping} /></button>
-          <button className="button-icon profile"><FontAwesomeIcon icon={faUser} /></button>
+          <button type="button" className="button-icon cart"><FontAwesomeIcon icon={faCartShopping} /></button>
+          <button type="button" className="button-icon profile"><FontAwesomeIcon icon={faUser} /></button>
         </div>
       </nav>
       <h1 id="title">Star Tech</h1>
