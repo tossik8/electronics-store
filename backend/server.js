26b0a951a59829fb99c505e9bc38295d111001e2
unknown <toropov.nikita2004@gmail.com>
2023-04-24 12:54:10 +0200

    feat: implement search for items

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,31 @@ const db = require("./db");
 
 const port = process.env.PORT || 3000;
 
-app.get("/api/v1/test", async (req, res) => {
-    console.log(await db.query("SELECT * FROM person", []));
+app.get("/api/v1/electronics-store", async (req, res) => {
+    try{
+        const { query } = req.query;
+        const keywords = query.split(" ");
+        keywords.splice(3)
+        keywords.forEach((keyword, i) => keywords[i] = "%" + keyword + "%");
+        let devices = {};
+        if(keywords.length === 3){
+            devices = await db.query("SELECT * FROM device WHERE (model ILIKE $3 AND model ILIKE $2 AND name ILIKE $1) OR (model ILIKE $1 AND model ILIKE $3 AND model ILIKE $2) OR (name ILIKE $1 AND name ILIKE $2 AND model ILIKE $3) OR (name ILIKE $2 AND name ILIKE $1 AND name ILIKE $3)", keywords);
+            keywords.splice(2);
+        }
+        if(keywords.length === 2 && (devices.rowCount === 0 || Object.keys(devices).length === 0)){
+            devices = await db.query("SELECT * FROM device WHERE name || ' ' || model ILIKE $1 || $2 OR (model ILIKE $2 AND name ILIKE $1) OR (name ILIKE $2 AND model ILIKE $1) OR (model ILIKE $2 AND model ILIKE $1)", keywords);
+            keywords.splice(1);
+        }
+        if(devices.rowCount === 0 || Object.keys(devices).length === 0){
+            devices = await db.query("SELECT * FROM device WHERE name || ' ' || model ILIKE $1 OR model ILIKE $1 OR name ILIKE $1", keywords);
+        }
+        res.json({
+            data: devices.rows
+        });
+    } catch (e) {
+        console.error(e);
+    }
+
 })
 
 app.listen(port, () => {
