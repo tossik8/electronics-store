44953fc1643c20aa4081826b3decced0aa40369b
unknown <toropov.nikita2004@gmail.com>
2023-05-02 21:41:16 +0200

    fix: style with a module

--- frontend/src/components/Categories.tsx
+++ frontend/src/components/Categories.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "../css/Categories.css"
+import styles from "../css/Categories.module.css"
 import { useNavigate } from 'react-router-dom'
 
 const Categories = () => {
@@ -11,28 +11,28 @@ const Categories = () => {
   }
 
   return (
-    <main id="categories-section">
-      <h2 className="categories-heading">Shop by category</h2>
-      <div className="categories-div">
-        <article className="category-article" onClick={() => handleClick("Phones")}>
-          <img src="/mobile-phone.jpg" loading="lazy" className="category-image" />
-          <p className="category-title">Phones</p>
-          <p className="description">High class, high quality phones at affordable prices</p>
+    <main id={styles.categories_section}>
+      <h2 className={styles.categories_heading}>Shop by category</h2>
+      <div className={styles.categories_div}>
+        <article className={styles.category_article} onClick={() => handleClick("Phones")}>
+          <img src="/mobile-phone.jpg" loading="lazy" className={styles.category_image} />
+          <p className={styles.category_title}>Phones</p>
+          <p className={styles.description}>High class, high quality phones at affordable prices</p>
         </article>
-        <article className="category-article" onClick={() => handleClick("Laptops")}>
-          <img src="/laptop.jpg" loading="lazy" className="category-image"/>
-          <p className="category-title">Laptops</p>
-          <p className="description">Cutting-edge laptops</p>
+        <article className={styles.category_article} onClick={() => handleClick("Laptops")}>
+          <img src="/laptop.jpg" loading="lazy" className={styles.category_image}/>
+          <p className={styles.category_title}>Laptops</p>
+          <p className={styles.description}>Cutting-edge laptops</p>
         </article>
-        <article className="category-article" onClick={() => handleClick("TVs")}>
-          <img src="/tv.jpg" loading="lazy" className="category-image"/>
-          <p className="category-title">TVs</p>
-          <p className="description">High-resolution TVs</p>
+        <article className={styles.category_article} onClick={() => handleClick("TVs")}>
+          <img src="/tv.jpg" loading="lazy" className={styles.category_image}/>
+          <p className={styles.category_title}>TVs</p>
+          <p className={styles.description}>High-resolution TVs</p>
         </article>
-        <article className="category-article" onClick={() => handleClick("Headphones")}>
-          <img src="/headphones.jpg" loading="lazy" className="category-image"/>
-          <p className="category-title">Headphones</p>
-          <p className="description">The sound quality you have never heard before</p>
+        <article className={styles.category_article} onClick={() => handleClick("Headphones")}>
+          <img src="/headphones.jpg" loading="lazy" className={styles.category_image}/>
+          <p className={styles.category_title}>Headphones</p>
+          <p className={styles.description}>The sound quality you have never heard before</p>
         </article>
       </div>
     </main>
