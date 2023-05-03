5ff5b42d68914ee68ed0a3327854b8b934fc9d57
unknown <toropov.nikita2004@gmail.com>
2023-05-03 12:02:58 +0200

    fix: make DevicesList.css a module

--- frontend/src/components/DevicesList.tsx
+++ frontend/src/components/DevicesList.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import Device from './Device'
 import { DevicesContext } from '../context/DevicesContext'
-import "../css/DevicesList.css"
+import styles from "../css/DevicesList.module.css"
 
 interface DevicesListProps{
     title: string | undefined,
@@ -10,9 +10,9 @@ interface DevicesListProps{
 const DevicesList = ({title} : DevicesListProps) => {
   const { devices } = useContext(DevicesContext);
   return (
-    <main id="devices">
-        <h1 id="category-title">{title}</h1>
-        <div className="devices-wrapper">
+    <main id={styles.devices}>
+        <h1 id={styles.category_title}>{title}</h1>
+        <div className={styles.devices_wrapper}>
             {devices.map(device => (
                 <Device key={device.id} id={device.id} name={device.name} model={device.model} url={device.url} price={device.price} description={device.description} category_id={device.category_id}/>
             ))}
