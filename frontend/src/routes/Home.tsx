0fbdba4a4454c57100483875cb452a2fd71286a6
unknown <toropov.nikita2004@gmail.com>
2023-05-03 11:46:35 +0200

    fix: make Home.css a module

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "../css/Home.css";
+import styles from "../css/Home.module.css";
 import Categories from '../components/Categories';
 import SoundButton from '../components/SoundButton';
 import Navigation from '../components/Navigation';
@@ -7,7 +7,7 @@ import Navigation from '../components/Navigation';
 const Home = () => {
 
   return (
-    <div id="home-page">
+    <div id={styles.home_page}>
       <Navigation source={"/FinalVideo.mp4"} title={"Star Tech"}/>
       <SoundButton/>
       <Categories/>
