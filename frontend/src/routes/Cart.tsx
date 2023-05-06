2287c4097081d0ff901b77ab4a0ed1efc4c22475
unknown <toropov.nikita2004@gmail.com>
2023-05-06 11:09:27 +0200

    feat: add the order summary

--- frontend/src/routes/Cart.tsx
+++ frontend/src/routes/Cart.tsx
@@ -18,12 +18,18 @@ const Cart = () => {
                 <img className={styles.device_image} width={200} src={device.url} alt="Device image."/>
                 <div className={styles.device_info}>
                   <p className={styles.device_title}>{device.name} {device.model}</p>
-                  <p className={styles.device_price}>{device.price}</p>
+                  <p className={styles.device_price}>{device.price}€</p>
                   <p className={styles.device_quantity}>{device.quantity}</p>
+                  <p className={styles.item_total}>{+device.price * device.quantity!}€</p>
+                  <button>Delete</button>
                 </div>
               </article>
             ))}
           </section>
+          <div className={styles.order}>
+            <p className={styles.total}>Total price: {cart.reduce((sum, device) => sum + (device.quantity! * +device.price), 0)}</p>
+            <button>Proceed to checkout</button>
+          </div>
         </div>
       </main>
     </>
