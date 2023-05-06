77b9a301866a49670d0a093f0f2c8ebba49ace05
unknown <toropov.nikita2004@gmail.com>
2023-05-06 12:12:31 +0200

    style: style the delete button

--- frontend/src/routes/Cart.tsx
+++ frontend/src/routes/Cart.tsx
@@ -2,6 +2,8 @@ import React, { useContext } from 'react'
 import styles from "../css/Cart.module.css"
 import Navigation from '../components/Navigation'
 import { DevicesContext } from '../context/DevicesContext'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 const Cart = () => {
   const  { cart, setCart } = useContext(DevicesContext);
@@ -10,24 +12,27 @@ const Cart = () => {
     <>
       <Navigation height={true}/>
       <main id={styles.cart_main}>
+        <h1>Cart</h1>
         <div className={styles.panel}>
           <section className={styles.cart_section}>
-            <h1>Cart</h1>
             {cart.map(device => (
-              <article key={device.id} className={styles.cart_article}>
-                <img className={styles.device_image} width={200} src={device.url} alt="Device image."/>
-                <div className={styles.device_info}>
-                  <p className={styles.device_title}>{device.name} {device.model}</p>
-                  <p className={styles.device_price}>{device.price}€</p>
-                  <p className={styles.device_quantity}>{device.quantity}</p>
-                  <p className={styles.item_total}>{+device.price * device.quantity!}€</p>
-                  <button>Delete</button>
-                </div>
-              </article>
+              <div key={device.id}>
+                <article className={styles.cart_article}>
+                  <img className={styles.device_image} src={device.url} alt="Device image."/>
+                  <div className={styles.device_info}>
+                    <p className={styles.device_title}>{device.name} {device.model}</p>
+                    <p className={styles.device_price}>Price: {device.price}€</p>
+                    <p className={styles.device_quantity}>Quantity: {device.quantity}</p>
+                    <p className={styles.item_total}>Total: {+device.price * device.quantity!}€</p>
+                    <button className={styles.delete}><FontAwesomeIcon className={styles.trash} icon={faTrash}/>Delete</button>
+                  </div>
+                </article>
+                <div className={styles.divider}/>
+              </div>
             ))}
           </section>
           <div className={styles.order}>
-            <p className={styles.total}>Total price: {cart.reduce((sum, device) => sum + (device.quantity! * +device.price), 0)}</p>
+            <p className={styles.total}>Total price: {cart.reduce((sum, device) => sum + (device.quantity! * +device.price), 0)}€</p>
             <button>Proceed to checkout</button>
           </div>
         </div>
