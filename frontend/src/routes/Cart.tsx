7bb73a671bcc5d17604956d0770f1e11d53f2fa1
unknown <toropov.nikita2004@gmail.com>
2023-05-06 10:34:19 +0200

    feat: display items in the cart

--- frontend/src/routes/Cart.tsx
+++ frontend/src/routes/Cart.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styles from "../css/Cart.module.css"
+import Navigation from '../components/Navigation'
+import { DevicesContext } from '../context/DevicesContext'
 
 const Cart = () => {
+  const  { cart, setCart } = useContext(DevicesContext);
+
   return (
-    <div>Cart</div>
+    <>
+      <Navigation height={true}/>
+      <main id={styles.cart_main}>
+        <div className={styles.panel}>
+          <section className={styles.cart_section}>
+            <h1>Cart</h1>
+            {cart.map(device => (
+              <article key={device.id} className={styles.cart_article}>
+                <img className={styles.device_image} width={200} src={device.url} alt="Device image."/>
+                <div className={styles.device_info}>
+                  <p className={styles.device_title}>{device.name} {device.model}</p>
+                  <p className={styles.device_price}>{device.price}</p>
+                  <p className={styles.device_quantity}>{device.quantity}</p>
+                </div>
+              </article>
+            ))}
+          </section>
+        </div>
+      </main>
+    </>
   )
 }
 
