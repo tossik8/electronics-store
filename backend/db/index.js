ebc1411489f17f6ea1ce419adc90e01e4abac35d
unknown <toropov.nikita2004@gmail.com>
2023-06-18 06:38:09 +0200

    feat: connect to a database with a connection string

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,6 +1,11 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
-const pool = new Pool();
+const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}?ssl=true`
+
+const pool = new Pool({
+    connectionString
+});
 
 module.exports = {
     query: (text, params) => pool.query(text, params)
