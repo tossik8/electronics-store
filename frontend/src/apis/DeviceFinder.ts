d657f063671180ef40a39a54f50a7376099995ca
unknown <toropov.nikita2004@gmail.com>
2023-06-18 09:35:09 +0200

    feat: send query requests to a remote server

--- frontend/src/apis/DeviceFinder.ts
+++ frontend/src/apis/DeviceFinder.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3000/api/v1/electronics-store"
+    baseURL: "https://electronics-store-server-k1rd.onrender.com/api/v1/electronics-store"
 })
