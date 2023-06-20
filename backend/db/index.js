847d078a3a2f9175d34adcf893deca8a12713068
unknown <toropov.nikita2004@gmail.com>
2023-06-20 11:28:34 +0200

    feat: connect to a remote database

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,6 +1,10 @@
 const { Pool } = require("pg");
 
-const pool = new Pool();
+const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}?ssl=true`
+
+const pool = new Pool({
+    connectionString
+});
 
 module.exports = {
     query: (text, params) => pool.query(text, params)
